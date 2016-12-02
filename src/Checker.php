f8fe6a372b58ac2e696f2c0f66b28523c23d7b2d
kduma <krystian@muzik.pl>
2016-12-02 16:22:33 +0000

    Apply fixes from StyleCI

--- src/Checker.php
+++ src/Checker.php
@@ -1,12 +1,11 @@
 <?php
-namespace KDuma\FileHasher;
 
+namespace KDuma\FileHasher;
 
 use MessagePack\Unpacker;
 
 /**
- * Class Checker
- * @package KDuma\FileHasher
+ * Class Checker.
  */
 class Checker
 {
@@ -15,28 +14,33 @@ class Checker
      * @return bool
      * @throws \Exception
      */
-    public static function file($hash_file) {
+    public static function file($hash_file)
+    {
         $path_info = pathinfo($hash_file);
 
-        if($path_info['extension'] != 'ph')
+        if ($path_info['extension'] != 'ph') {
             throw new \Exception('Unsupported hash file!');
+        }
 
         $hash_file = realpath($path_info['dirname'].'/'.$path_info['basename']);
         $real_file = realpath($path_info['dirname'].'/'.$path_info['filename']);
 
-        if(!$hash_file)
+        if (! $hash_file) {
             throw new \Exception('Hash file doesn\'t exist!');
+        }
 
-        if(!$real_file)
+        if (! $real_file) {
             throw new \Exception('Hashed file doesn\'t exist!');
+        }
 
         $hashes = (new Unpacker())->unpack(file_get_contents($hash_file));
 
         $sha1 = sha1_file($real_file);
         $md5 = md5_file($real_file);
 
-        if($sha1 != $hashes['sha1'] || $md5 != $hashes['md5'])
+        if ($sha1 != $hashes['sha1'] || $md5 != $hashes['md5']) {
             return false;
+        }
 
         return true;
     }
--- src/Hasher.php
+++ src/Hasher.php
@@ -1,12 +1,11 @@
 <?php
-namespace KDuma\FileHasher;
 
+namespace KDuma\FileHasher;
 
 use MessagePack\Packer;
 
 /**
- * Class Hasher
- * @package KDuma\FileHasher
+ * Class Hasher.
  */
 class Hasher
 {
@@ -15,14 +14,16 @@ class Hasher
      * @return bool
      * @throws \Exception
      */
-    public static function file($real_file) {
+    public static function file($real_file)
+    {
         $path_info = pathinfo($real_file);
 
         $real_file = realpath($path_info['dirname'].'/'.$path_info['basename']);
         $hash_file = $real_file.'.ph';
 
-        if(!$real_file)
+        if (! $real_file) {
             throw new \Exception('Hashed file doesn\'t exist!');
+        }
 
         $sha1 = sha1_file($real_file);
         $md5 = md5_file($real_file);
