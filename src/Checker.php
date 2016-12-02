5f253bff159cc56bb0c8e2b0b0a49a1d4b9d3c32
Krystian Duma <git@kduma.tk>
2016-12-02 17:14:00 +0100

    First Version

---
+++ src/Checker.php
@@ -0,0 +1,43 @@
+<?php
+namespace KDuma\FileHasher;
+
+
+use MessagePack\Unpacker;
+
+/**
+ * Class Checker
+ * @package KDuma\FileHasher
+ */
+class Checker
+{
+    /**
+     * @param $hash_file
+     * @return bool
+     * @throws \Exception
+     */
+    public static function file($hash_file) {
+        $path_info = pathinfo($hash_file);
+
+        if($path_info['extension'] != 'ph')
+            throw new \Exception('Unsupported hash file!');
+
+        $hash_file = realpath($path_info['dirname'].'/'.$path_info['basename']);
+        $real_file = realpath($path_info['dirname'].'/'.$path_info['filename']);
+
+        if(!$hash_file)
+            throw new \Exception('Hash file doesn\'t exist!');
+
+        if(!$real_file)
+            throw new \Exception('Hashed file doesn\'t exist!');
+
+        $hashes = (new Unpacker())->unpack(file_get_contents($hash_file));
+
+        $sha1 = sha1_file($real_file);
+        $md5 = md5_file($real_file);
+
+        if($sha1 != $hashes['sha1'] || $md5 != $hashes['md5'])
+            return false;
+
+        return true;
+    }
+}
---
+++ src/Hasher.php
@@ -0,0 +1,34 @@
+<?php
+namespace KDuma\FileHasher;
+
+
+use MessagePack\Packer;
+
+/**
+ * Class Hasher
+ * @package KDuma\FileHasher
+ */
+class Hasher
+{
+    /**
+     * @param $real_file
+     * @return bool
+     * @throws \Exception
+     */
+    public static function file($real_file) {
+        $path_info = pathinfo($real_file);
+
+        $real_file = realpath($path_info['dirname'].'/'.$path_info['basename']);
+        $hash_file = $real_file.'.ph';
+
+        if(!$real_file)
+            throw new \Exception('Hashed file doesn\'t exist!');
+
+        $sha1 = sha1_file($real_file);
+        $md5 = md5_file($real_file);
+
+        file_put_contents($hash_file, (new Packer())->pack(['sha1' => $sha1, 'md5' => $md5]));
+
+        return true;
+    }
+}
